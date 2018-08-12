7d68196c0e55f92a9a47dfbb9c4ba12e07e659ba
Danilo Pimentel <danilo.pimentel@smarttbot.com>
2018-08-12 03:36:11 -0300

    Add link to the stock detail on the card.

--- src/components/StockCard/StockCard.tsx
+++ src/components/StockCard/StockCard.tsx
@@ -2,6 +2,7 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import * as React from 'react';
+import { Link } from 'react-router-dom'
 import { IStock } from '../../ducks/stocks';
 import classes from './StockCard.scss';
 
@@ -13,29 +14,34 @@ class StockCard extends React.Component<any, IStockCardProps> {
   public render() {
     const stock = this.props.stock;
     return (
-      <Card className={classes.StockCard}>
-        <CardContent>
-          <Typography
-            className={classes.StockCardCompanyName}
-            color="textSecondary"
-          >
-            {stock.companyName}
-          </Typography>
-          <Typography
-            className={classes.StockCardCode}
-            variant="headline"
-            component="h2"
-          >
-            {stock.symbol}
-          </Typography>
-          <Typography
-            className={classes.StockCardSector}
-            color="textSecondary"
-          >
-            {stock.sector}
-          </Typography>
-        </CardContent>
-      </Card>
+      <Link
+        to={`/stock?symbol=${stock.symbol}`}
+        className={classes.StockCardLink}
+      >
+        <Card className={classes.StockCard}>
+          <CardContent>
+            <Typography
+              className={classes.StockCardCompanyName}
+              color="textSecondary"
+            >
+              {stock.companyName}
+            </Typography>
+            <Typography
+              className={classes.StockCardCode}
+              variant="headline"
+              component="h2"
+            >
+              {stock.symbol}
+            </Typography>
+            <Typography
+              className={classes.StockCardSector}
+              color="textSecondary"
+            >
+              {stock.sector}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Link>
     );
   }
 }
