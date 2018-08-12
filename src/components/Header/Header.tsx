4ea109ba932144bc7f40cf5f9cb15eea04d11363
Danilo Pimentel <danilo.pimentel@smarttbot.com>
2018-08-12 03:45:52 -0300

    Add link on the header to the home page.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,16 @@
 import Typography from '@material-ui/core/Typography';
 import * as React from 'react';
+import { Link } from 'react-router-dom'
+import logo from '../../assets/iex-logo.png';
 import classes from './Header.scss';
 
 class Header extends React.Component {
   public render() {
     return (
       <header className={classes.Header}>
+        <Link to="/" title="Go to home" className={classes.HeaderLink}>
+          <img src={logo} alt="IEX Trading" className={classes.HeaderLogo} />
+        </Link>
         <Typography
           className={classes.HeaderTitle}
           variant="display3"
--- src/components/StockCard/StockCard.tsx
+++ src/components/StockCard/StockCard.tsx
@@ -2,7 +2,7 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import * as React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import { IStock } from '../../ducks/stocks';
 import classes from './StockCard.scss';
 
